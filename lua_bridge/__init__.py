0ec8244059a879cf6ecba629a6b508a503fb58a6
Ayuto22 <ayuto@web.de>
2016-09-25 09:36:01 +0200

    Added data for global lua interface pointer

--- lua_bridge/__init__.py
+++ lua_bridge/__init__.py
@@ -1,8 +1,13 @@
 # =============================================================================
 # >> IMPORTS
 # =============================================================================
+# Source.Python
+import memory
+
+from core import PLATFORM
+
 # Lua Bridge
-from _lua_bridge import *
+from lua_bridge._lua_bridge import *
 
 
 # =============================================================================
@@ -14,4 +19,22 @@ __all__ = (
     'LuaType',
     'LuaState',
     'UserData'
+    'lua',
 )
+
+
+# =============================================================================
+# >> GLOBAL VARIABLES
+# =============================================================================
+# TODO: Move this into a file
+server = memory.find_binary('server')
+
+if PLATFORM == 'windows':
+    pLua = server.find_pointer(
+        b'\x55\x8B\xEC\x6A\x01\xE8\x2A\x2A\x2A\x2A\x83\xC4\x04\x84\xC0\x74'
+        b'\x2A\x83\x3D\x2A\x2A\x2A\x2A\x00\x74\x2A\x56\x8B\x75\x08\x83\x3E\x02',
+        84, 2)
+else:
+    pLua = server.find_pointer('g_Lua', level=1)
+
+lua = memory.make_object(LuaBase, pLua)
